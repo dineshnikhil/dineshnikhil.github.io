6790263c4a588ddc5908c3d01f21402b059b29d8
dineshnikhil <dineshkumarpokkula@gmail.com>
2022-08-15 14:55:35 +0530

    added the light thema and sound to the website

--- src/components/nav/Nav.jsx
+++ src/components/nav/Nav.jsx
@@ -1,9 +1,69 @@
 import React from 'react'
+import { useState } from 'react'
 import classes from './Nav.module.css'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faGear } from '@fortawesome/free-solid-svg-icons'
+import { faSun } from '@fortawesome/free-solid-svg-icons'
+import { faMoon } from '@fortawesome/free-solid-svg-icons'
+import { faVolumeHigh } from '@fortawesome/free-solid-svg-icons'
+import { faVolumeMute } from '@fortawesome/free-solid-svg-icons'
+
+import clickSound from '../../audio/click.wav'
+import soundOn from '../../audio/soundOn.wav'
+import soundOff from '../../audio/soundOff.wav'
 
 function Nav() {
+
+  const [icon, setIcon] = useState(faSun);
+  const [speakerIcon, setSpeakerIcon] = useState(faVolumeHigh);
+  const [mode, setMode] = useState(true);
+  const [muted, setMuted] = useState(false);
+
+  const clickSnd = new Audio(clickSound)
+  const soundOnSnd = new Audio(soundOn)
+  const soundOffSnd = new Audio(soundOff)
+
+  function startSound() {
+    if(muted) {
+      clickSnd.played();
+    } else {
+      clickSnd.play();
+    }
+  }
+
+  function soundOnHandler() {
+    soundOnSnd.play();
+  }
+
+  function soundOffHandler() {
+    soundOffSnd.play();
+  }
+
+  function modeChangeHandler() {
+    if(mode) {
+      startSound();
+      document.documentElement.setAttribute('data-theme', 'light');
+      setIcon(faMoon);
+      setMode(false)
+    } else {
+      startSound();
+      document.documentElement.setAttribute('data-theme', 'dark');
+      setIcon(faSun);
+      setMode(true)
+    }
+  }
+
+  function muteHandler() {
+    if(muted) {
+      setMuted(false);
+      setSpeakerIcon(faVolumeHigh);
+      soundOnHandler()
+    } else {
+      setMuted(true);
+      setSpeakerIcon(faVolumeMute);
+      soundOffHandler()
+    }
+  }
+
   return (
     <nav>
         <div className={classes.navLinks}>
@@ -12,8 +72,11 @@ function Nav() {
             <a href="#">Fun Talk</a>
         </div>
         <div className={classes.setting}>
-            <button>
-                <FontAwesomeIcon icon={faGear} />
+            <button onClick={modeChangeHandler}>
+                <FontAwesomeIcon icon={icon} />
+            </button>
+            <button onClick={muteHandler}>
+              <FontAwesomeIcon icon={speakerIcon} />
             </button>
         </div>
     </nav>
